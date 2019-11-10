397b2c91cc06f4490c73e13451a02de51b90a42f
Quinn Bowers <qbowers@mit.edu>
2019-11-09 22:23:54 -0500

    external git scripts

--- pull_dist.bat
+++ pull_dist.bat
@@ -1,10 +1,10 @@
 @ECHO Off
-IF EXIST "battlecode20-dist" (
+IF EXIST "dist" (
     ::If it does, yeet in and git pull
-    cd battlecode20-dist
+    cd dist
     git pull "ext::ssh -i ../.ssh/id_rsa git@github.com %%S battlecode/battlecode20-dist"
 ) ELSE (
     ::If directory doesnt exist, clone it
-    git clone "ext::ssh -i .ssh/id_rsa git@github.com %%S battlecode/battlecode20-dist"
+    git clone "ext::ssh -i .ssh/id_rsa git@github.com %%S battlecode/battlecode20-dist" dist
 )
 
