08881f2c38a7f2c920eaf0e3fcd77f43e1cf6e6a
Nate Foss <npfoss@gmail.com>
2020-01-07 18:12:30 -0500

    make examplefuncs use all dirs

--- src/examplefuncsplayer/RobotPlayer.java
+++ src/examplefuncsplayer/RobotPlayer.java
@@ -4,7 +4,16 @@ import battlecode.common.*;
 public strictfp class RobotPlayer {
     static RobotController rc;
 
-    static Direction[] directions = {Direction.NORTH, Direction.EAST, Direction.SOUTH, Direction.WEST};
+    static Direction[] directions = {
+        Direction.NORTH,
+        Direction.NORTHEAST,
+        Direction.EAST,
+        Direction.SOUTHEAST,
+        Direction.SOUTH,
+        Direction.SOUTHWEST,
+        Direction.WEST,
+        Direction.NORTHWEST
+    };
     static RobotType[] spawnedByMiner = {RobotType.REFINERY, RobotType.VAPORATOR, RobotType.DESIGN_SCHOOL,
             RobotType.FULFILLMENT_CENTER, RobotType.NET_GUN};
 
